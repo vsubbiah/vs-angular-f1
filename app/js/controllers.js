2e24775e6f8b3d66af237c4fe67efddcf4c655db
Venkat Subbiah <v.subbiah@gmail.com>
2015-11-05 22:38:29 -0800

    Using ergstatAPI

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,27 +1,11 @@
 angular.module('F1FeederApp.controllers', []).
-controller('driversController', function($scope) {
-    $scope.driversList = [
-      {
-          Driver: {
-              givenName: 'Sebastian',
-              familyName: 'Vettel'
-          },
-          points: 322,
-          nationality: "German",
-          Constructors: [
-              {name: "Red Bull"}
-          ]
-      },
-      {
-          Driver: {
-          givenName: 'Fernando',
-              familyName: 'Alonso'
-          },
-          points: 207,
-          nationality: "Spanish",
-          Constructors: [
-              {name: "Ferrari"}
-          ]
-      }
-    ];
-});
+  controller('driversController', function($scope, ergastAPIservice) {
+    $scope.nameFilter = null;
+    $scope.driversList = [];
+
+    ergastAPIservice.getDrivers().success(function (response) {
+        //Dig into the responde to get the relevant data
+        $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    });
+  });
+
